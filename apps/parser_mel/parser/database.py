00c34fe8f3adbe3e6b6874101f0b6c7887f69143
iriskin77 <vlad34b8@gmail.com>
2024-01-01 11:47:28 +0500

    added methods to insert data into db

---
+++ apps/parser_mel/parser/database.py
@@ -0,0 +1,37 @@
+import logging
+from apps.parser_tink.models import Article, Category, Author
+
+
+class Database:
+
+    def __init__(self):
+        self.logger = logging.getLogger('main')
+
+    def insert_authors(self, json_articles: dict) -> None:
+
+        for article in json_articles['Mel_articles']:
+                author_check = Author.objects.filter(author=article['author']).exists()
+                if not author_check:
+                    Author.objects.create(author=article['author']).save()
+
+    def insert_articles(self, json_articles: dict) -> None:
+
+         for article in json_articles['Mel_articles']:
+
+                cat_name = Category.objects.filter(name_cat=json_articles['Mel_cat']).first()
+                author = Author.objects.filter(author=article['author']).first()
+
+                article_check = Article.objects.filter(title=article['title']).exists()
+
+                if not article_check:
+
+                    new_text = Article.objects.create(
+                                         category=cat_name,
+                                         author=author,
+                                         title=article['title'],
+                                         body=article['body'],
+                                         date_published=article['date_published'],
+                                         link=article['link_article']
+                    )
+                    new_text.save()
+
