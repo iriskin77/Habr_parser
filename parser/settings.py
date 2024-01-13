6f3c84423aa55c1f426e5c7612aa53c9d9286b55
iriskin77 <vlad34b8@gmail.com>
2024-01-13 18:49:31 +0500

    Add: redis host changed in settings.py

--- parser/settings.py
+++ parser/settings.py
@@ -140,8 +140,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CELERY_BROKER_URL = "redis://redis:6379/0"
 # CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 
 LOGGING = {
