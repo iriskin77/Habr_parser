9fb676e3a67d21b0279eaea98e77409f68f6b06b
iriskin77 <vlad34b8@gmail.com>
2024-02-01 12:05:07 +0500

    Add: prometheus in database and postgres

--- parser/settings.py
+++ parser/settings.py
@@ -93,17 +93,24 @@ WSGI_APPLICATION = "parser.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        # "NAME": "files",
-        # "USER": "r2",
-        # "PASSWORD": "password",
-        # 'HOST': '127.0.0.1',
-        # "PORT": 5432,
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django_prometheus.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv('POSTGRES_DB', 'db'),
+            "USER": os.getenv('POSTGRES_USER', 'postgres'),
+            "PASSWORD": os.getenv('POSTGRES_PASSWORD', 'postgres'),
+            "HOST": os.getenv('POSTGRES_HOST', 'db'),
+            "PORT": os.getenv('POSTGRES_PORT', 5432),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
